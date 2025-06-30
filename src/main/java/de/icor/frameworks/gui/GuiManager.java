c9845e5dd74544bbab4d69a8189600299199bcfc
coreystndr <coreystndr@gmail.com>
2025-06-30 20:46:10 +0200

    1.0.0

--- src/main/java/de/icor/frameworks/gui/GuiManager.java
+++ src/main/java/de/icor/frameworks/gui/GuiManager.java
@@ -9,24 +9,10 @@ import java.util.HashMap;
 import java.util.UUID;
 
 public class GuiManager implements Listener {
-    private final HashMap<UUID, GuiBuilder> openGuis = new HashMap<>();
+    public static final HashMap<UUID, GuiBuilder> openGuis = new HashMap<>();
 
     public void registerGui(UUID player, GuiBuilder gui) {
         openGuis.put(player, gui);
-    }
-
-    @EventHandler
-    public void onInventoryClick(InventoryClickEvent event) {
-        UUID player = event.getWhoClicked().getUniqueId();
-        if (openGuis.containsKey(player)) {
-            openGuis.get(player).handleClick(event);
-        }
-    }
-
-    @EventHandler
-    public void onInventoryClose(InventoryCloseEvent event) {
-        UUID player = event.getPlayer().getUniqueId();
-        openGuis.remove(player);
     }
 
     public GuiBuilder getGui(UUID player) {
