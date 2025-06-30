1da00d4eb4e88ad28fe3868a61bda118b1afef39
coreystndr <coreystndr@gmail.com>
2025-06-30 21:08:36 +0200

    1.0.1

--- src/main/java/de/icor/frameworks/Frameworks.java
+++ src/main/java/de/icor/frameworks/Frameworks.java
@@ -2,8 +2,10 @@ package de.icor.frameworks;
 
 import de.icor.frameworks.gui.*;
 import lombok.Getter;
+import lombok.Setter;
 
 public final class Frameworks {
+    @Setter
     @Getter
     private static GuiManager guiManager = new GuiManager();
 }
--- src/main/java/de/icor/frameworks/gui/GuiManager.java
+++ src/main/java/de/icor/frameworks/gui/GuiManager.java
@@ -9,10 +9,24 @@ import java.util.HashMap;
 import java.util.UUID;
 
 public class GuiManager implements Listener {
-    public static final HashMap<UUID, GuiBuilder> openGuis = new HashMap<>();
+    private final HashMap<UUID, GuiBuilder> openGuis = new HashMap<>();
 
     public void registerGui(UUID player, GuiBuilder gui) {
         openGuis.put(player, gui);
+    }
+
+    @EventHandler
+    public void onInventoryClick(InventoryClickEvent event) {
+        UUID player = event.getWhoClicked().getUniqueId();
+        if (openGuis.containsKey(player)) {
+            openGuis.get(player).handleClick(event);
+        }
+    }
+
+    @EventHandler
+    public void onInventoryClose(InventoryCloseEvent event) {
+        UUID player = event.getPlayer().getUniqueId();
+        openGuis.remove(player);
     }
 
     public GuiBuilder getGui(UUID player) {
